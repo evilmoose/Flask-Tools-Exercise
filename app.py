4d9c3b6299bd0c23945c48626a7358c28e8b39af
Evilmoose <ronnielopez@gmail.com>
2023-06-29 09:25:55 -0700

    step 3

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
+
+from surveys import Survey, satisfaction_survey as survey
 
 app = Flask(__name__)
 
@@ -16,4 +18,9 @@ responses = []
 @app.route('/')
 def show_start_page():
     # Render start page with title and instructions
-    return render_template("start.html", survey=surveys["satisfaction"])
+    return render_template("start.html", survey=Survey["satisfaction"])
+
+@app.route('/questions/<int:qid>')
+def show_question(qid):
+    question = Survey["satisfaction"].questions[qid]
+    return render_template("question.html", question=question)
